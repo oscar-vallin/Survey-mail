be74ce6ea0cbcd254d0647e860a312c109df1569
Oscar vallin <arnoldvallin@gmail.com>
2020-11-16 17:10:26 -0600

    fix heroku

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,11 @@
+
 const passport = require('passport');
 
 module.exports = app => {
+    app.get("/", (req,res) => {
+        res.json({"name": "Karina"});
+    });
+
     //auth google
     app.get("/auth/google", passport.authenticate('google', {
         scope: ['profile', 'email'],
