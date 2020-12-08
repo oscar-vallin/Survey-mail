dc64b1915fd69e4f54724e4b02dde4e87d689e82
Oscar vallin <arnoldvallin@gmail.com>
2020-12-08 14:37:15 -0600

    fix error about express static

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ require('./routes/billingRoutes')(app);
 if(process.env.NODE_ENV === 'production'){
     //Express will server up production assets
     //like our main.js file, or main.css file
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
 
     //Express will server up the index.html file
     //I fit does not reconize the route
